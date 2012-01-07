d2504b94d814523e9f6946f9f677e472ee434ed3
Borja Sotomayor <borja@cs.uchicago.edu>
2012-01-07 13:17:04 -0600

    Fixed typos in score specification

--- tests/scores.py
+++ tests/scores.py
@@ -28,8 +28,6 @@ PROJECTS = [PROJ_1A, PROJ_1B, PROJ_1C]
 
 PROJ_1A.add_category("BASIC_CONNECTION", "Basic Connection", 50)
 
-PROJ_1B.add_category("BASIC_CONNECTION", "Basic Connection", 50)
-
 PROJ_1B.add_category("CONNECTION_REGISTRATION", "Connection Registration", 40)
 PROJ_1B.add_category("PRIVMSG_NOTICE", "PRIVMSG and NOTICE", 30)
 PROJ_1B.add_category("PING_PONG", "PING and PONG", 2.5)
@@ -39,7 +37,7 @@ PROJ_1B.add_category("WHOIS", "WHOIS", 10)
 PROJ_1B.add_category("ERR_UNKNOWN", "ERR_UNKNOWN", 2.5)
     
 PROJ_1C.add_category("CHANNEL_JOIN", "JOIN", 15)
-PROJ_1B.add_category("CHANNEL_PRIVMSG_NOTICE", "PRIVMSG and NOTICE to channels", 15)
+PROJ_1C.add_category("CHANNEL_PRIVMSG_NOTICE", "PRIVMSG and NOTICE to channels", 15)
 PROJ_1C.add_category("CHANNEL_PART", "PART", 10)
 PROJ_1C.add_category("CHANNEL_TOPIC", "TOPIC", 10)
 PROJ_1C.add_category("MODES", "User and channel modes", 25)
