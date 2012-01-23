f7f585bba7ff6fa772efc9f4ca71b3e473ef0c4b
Borja Sotomayor <borja@cs.uchicago.edu>
2012-01-22 23:17:11 -0600

    Removed old files that didn't actually run tests

--- tests/test_quit.py
+++
@@ -1,9 +0,0 @@
-import tests.replies as replies
-import time
-from tests.common import ChircTestCase, ChircClient
-
-class FOO(ChircTestCase):
-
-    def test_foo(self):
-        self._connect_user("user1", "User One")
-        
